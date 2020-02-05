f7bfe70607583414038c921c69167f47047b49dd
gnehs <jayuiop@gmail.com>
2020-02-05 20:07:51 +0800

    修正轉換時會超出限制

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const path = require('path'); // 引入路徑處理模組
 const inputDir = __dirname + "/input/"
 const outputDir = __dirname + "/output/"
+const tempDir = __dirname + "/temp/"
 async function main() {
     fs.readdir(inputDir, async (err, files) => { //讀取資料夾
         if (err) throw err;
@@ -22,8 +23,8 @@ async function parseFile(fileName) {
     let imgData, psdTempFile, fileSaveName
     if (fileName.endsWith('.psd')) {
         let psd = PSD.fromFile(path.resolve(inputDir, fileName))
-        psdTempFile = inputDir + Math.random().toString(36).substring(7); + '_temp.png'
-        await psd.parse();
+        psdTempFile = tempDir + Math.random().toString(36).substring(7) + '_temp.png'
+        await psd.parse()
         await psd.image.saveAsPng(psdTempFile)
         imgData = sharp(psdTempFile)
     } else {
@@ -33,35 +34,38 @@ async function parseFile(fileName) {
     // 縮放大小
     let metadata = await imgData.metadata()
     console.log(`🚂 正在轉換 ${fileName} (${metadata.width}x${metadata.height})`)
-    let resizeOptions = {
-        height: 310
-    }
-    if (metadata.width < metadata.height)
-        resizeOptions = {
-            width: 360
+    imgData = await imgData.trim().resize(310, 360, {
+        fit: 'inside',
+        background: {
+            r: 0,
+            g: 0,
+            b: 0,
+            alpha: 0
         }
-    imgData = await imgData.trim().resize(resizeOptions).toBuffer()
+    }).trim().toBuffer()
     imgData = sharp(imgData)
 
     // 擴張
     metadata = await imgData.metadata()
-    await imgData
-        .extend({
-            top: 5,
-            bottom: metadata.height % 2 == 0 ? 5 : 4,
-            left: metadata.width % 2 == 0 ? 5 : 4,
-            right: 5,
-            background: {
-                r: 0,
-                g: 0,
-                b: 0,
-                alpha: 0
-            }
-        })
-        .toFile(outputDir + fileSaveName);
+    imgData = await imgData.extend({
+        top: 5,
+        bottom: metadata.height % 2 == 0 ? 5 : 4,
+        left: metadata.width % 2 == 0 ? 5 : 4,
+        right: 5,
+        background: {
+            r: 0,
+            g: 0,
+            b: 0,
+            alpha: 0
+        }
+    }).toBuffer()
+    imgData = sharp(imgData)
+    metadata = await imgData.metadata()
+
+    await imgData.toFile(outputDir + fileSaveName);
     if (fileName.endsWith('.psd')) {
         await fs.unlinkSync(psdTempFile);
     }
-    console.log(`✅ 轉換完畢 ${fileName}`)
+    console.log(`✅ 轉換完畢 ${fileName} (${metadata.width}x${metadata.height})`)
 }
 main()
